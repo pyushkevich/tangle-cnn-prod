c2d5581163490d04baeb864d9744db4689da4e40
Paul Yushkevich <pyushkevich@gmail.com>
2020-05-06 20:19:03 +0000

    added training code

--- scan_tangles.py
+++ scan_tangles.py
@@ -283,11 +283,154 @@ def do_apply(args):
     sitk.WriteImage(nii, args.output)
 
 
+
+# Standard training code
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        estart = time.time()
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                labels_one_hot = torch.zeros([labels.shape[0], 2])
+                labels_one_hot[:,0] = (labels==0)
+                labels_one_hot[:,1] = (labels==1)
+                labels_one_hot = labels_one_hot.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels_one_hot)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f} Time: {:8.4f}'.format(phase, epoch_loss, epoch_acc, (time.time()-estart)))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+
+def do_train(args):
+
+    # Number of classes in the dataset
+    num_classes = 2
+
+    # Batch size for training (change depending on how much memory you have)
+    batch_size = int(args.batch)
+    num_epochs = int(args.epochs)
+
+    # Input image size
+    input_size = 224
+
+    # Initialize the wildcat model
+    model=resnet50_wildcat_upsample(2, pretrained=True, kmax=0.02, kmin=0.0, alpha=0.7, num_maps=4)
+
+    # Loss and optimizer
+    criterion = nn.MultiLabelSoftMarginLoss()
+    optimizer = torch.optim.SGD(model.get_config_optim(0.01, 0.1), lr=0.01, momentum=0.9, weight_decay=1e-4)
+
+    # Transforms for training and validation
+    data_transforms = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(input_size),
+                transforms.RandomRotation(45),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ]),
+            'val': transforms.Compose([
+                transforms.Resize(input_size),
+                transforms.CenterCrop(input_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ]),
+            }
+
+    # Training and validation dataloaders
+    image_datasets = {x: datasets.ImageFolder(os.path.join(args.datadir, x), data_transforms[x]) for x in ['train', 'val']}
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+
+    # Map to CUDA
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
+
+    model = model.to(device)
+    criterion = criterion.to(device)
+
+    # Run the training
+    model_ft, hist = train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
+
+    # After the training, save the model
+    torch.save(model_ft.state_dict(), args.output)
+
+
+
 # Set up an argument parser
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
 train_parser = subparsers.add_parser('train')
+train_parser.add_argument('--datadir', help='source directory')
+train_parser.add_argument('--output', help='output classifier')
+train_parser.add_argument('--epochs', help='number of epochs')
+train_parser.add_argument('--batch', help='batch size')
+train_parser.set_defaults(func=do_train)
+
 
 apply_parser = subparsers.add_parser('apply')
 apply_parser.add_argument('--slide', help='Input histology slide to process')
