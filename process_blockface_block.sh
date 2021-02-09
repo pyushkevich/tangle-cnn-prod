00688be06085a508d2867c3cf76239c9119e55ea
Paul Yushkevich <pyushkevich@gmail.com>
2021-02-09 14:57:18 +0000

    added check for existing files

--- process_blockface_block.sh
+++ process_blockface_block.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -x -e
+#set -x -e
 
 project_id=${1?}
 id=${2?}
 block=${3?}
+force=${4}
 
 # Activate service account
 if [[ -f /var/secrets/google/key.json ]]; then
@@ -11,14 +12,39 @@ if [[ -f /var/secrets/google/key.json ]]; then
   gcloud config set project $project_id
 fi
 
-# Download the SVS
-mkdir -p ./data ./model
+# Get a list of inputs and outputs found
+FINPUTS=$(mktemp /tmp/inputs.XXXXXX)
+FOUTPUTS=$(mktemp /tmp/outputs.XXXXXX)
+FNEEDED=$(mktemp /tmp/needed.XXXXXX)
 for slice in 05 10; do
-  if ! gsutil -m cp "gs://mtl_histology/$id/bf_raw/${id}_${block}_*_${slice}.jpg" ./data/; then
-    echo "Download of blockface JPEG file failed for $id $block"
-    exit 255
+  gsutil ls "gs://mtl_histology/$id/bf_raw/${id}_${block}_*_${slice}.jpg" >> $FINPUTS
+done
+
+if ! gsutil ls "gs://mtl_histology/$id/bf_proc/${id}_${block}_**/*.nii.gz" > $FOUTPUTS; then
+  touch $FOUTPUTS
+fi
+
+# Filter the inputs
+for fn in $(cat $FINPUTS); do
+  base=$(basename $fn .jpg)
+  if ! grep "${base}_deepcluster.nii.gz" $FOUTPUTS > /dev/null; then 
+    echo $fn >> $FNEEDED
+  elif ! grep "${base}_deepcluster_rgb.nii.gz" $FOUTPUTS > /dev/null; then 
+    echo $fn >> $FNEEDED
   fi
 done
+
+# Which file to use
+if [[ $force -gt 0 ]]; then
+  FNEEDED=$FINPUTS
+fi
+
+# Download the SVS
+mkdir -p ./data ./model
+if ! gsutil -m cp $(cat $FNEEDED) ./data/; then
+  echo "Download of blockface JPEG file failed for $id $block"
+  exit 255
+fi
 
 # Form the model URL
 MODEL_URL="gs://svsbucket/cnn_models/blockface/deepcluster.alexnet.tar"
